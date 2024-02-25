46270c83c72191476f71e212fe19a5ee95141f78
rebdiaz <95255349+rebdiaz@users.noreply.github.com>
2024-02-25 13:30:46 -0500

    Simple setup

---
+++ src/frontend/yumlink/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <script>
+          </script>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. Yay
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
