6ca4f53d64dc35ee10c12e6bab605dec4671af0e
amet407 <ametoje.virdi@gmail.com>
2024-03-27 19:35:02 -0400

    Connected backend to logins

--- src/frontend/yumlink/src/components/LoginSignup/LoginSignup.jsx
+++ src/frontend/yumlink/src/components/LoginSignup/LoginSignup.jsx
@@ -5,6 +5,7 @@ import './LoginSignup.css'
 import user_icon from '../Assets/person.png'
 import email_icon from '../Assets/email.png'
 import password_icon from '../Assets/password.png'
+import axios from "axios";
 
 const LoginSignup = () => {
     const history = useHistory();
@@ -31,6 +32,56 @@ const LoginSignup = () => {
         }
     };
 
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.post(
+                "http://localhost:3001/signup",
+                {password: password, username: email, name: name},
+                { withCredentials: true }
+            );
+            const { success, message } = data;
+            if (success) {
+                console.log("User added");
+                console.log(message);
+                history.push("/home");
+                // setTimeout(() => {
+                //     navigate("/");
+                // }, 1000);
+            } else {
+                console.log("Error adding user!")
+                console.log(message);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.post(
+                "http://localhost:3001/login",
+                {username: email, password: password},
+                { withCredentials: true }
+            );
+            const { success, message } = data;
+            if (success) {
+                console.log("Logged in!");
+                console.log(message);
+                history.push("/home");
+                // setTimeout(() => {
+                //     navigate("/");
+                // }, 1000);
+            } else {
+                console.log("Error logging in!")
+                console.log(message);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className='container'>
             <div className='header'>
@@ -41,27 +92,31 @@ const LoginSignup = () => {
                 {action === "Login" ? null : (
                     <div className="input">
                         <img src={user_icon} alt=""/>
-                        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                        <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
                     </div>
                 )}
-                
+
                 <div className="input">
                     <img src={email_icon} alt=""/>
-                    <input type="email" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="email" placeholder="Email Address" value={email}
+                           onChange={(e) => setEmail(e.target.value)}/>
                 </div>
 
                 <div className="input">
                     <img src={password_icon} alt=""/>
-                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <input type="password" placeholder="Password" value={password}
+                           onChange={(e) => setPassword(e.target.value)}/>
                 </div>
+
             </div>
             {action === "Sign Up" ? null : (
                 <div className="forgot-password">Lost Password? <span>Click Here!</span></div>
             )}
             <div className="submit-container">
-                
-                <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => handleActionChange("Sign Up")}>Sign Up</div>
-                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => handleActionChange("Login")}>Login</div>
+
+                <div className={action === "Login" ? "submit gray" : "submit"} onClick={handleSignup}>Sign Up</div>
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={handleLogin}>Login</div>
+
             </div>
         </div>
     );
