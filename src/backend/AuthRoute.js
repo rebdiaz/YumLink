b77f833e7a348a7d0e9fb7d393a28d0a53f51a2b
rebdiaz <95255349+rebdiaz@users.noreply.github.com>
2024-04-07 14:50:38 -0400

    updated routes with backend post request for creating complaints

--- src/backend/AuthRoute.js
+++ src/backend/AuthRoute.js
@@ -3,10 +3,12 @@
 const { Signup } = require("./SignUp");
 const { Authorize } = require("./Authorization");
 const {CreateListing} = require("./CreateListings");
+const {CreateComplaint} = require("./CreateComplaints");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
 router.post("/login", Authorize);
 router.post("/createlisting", CreateListing);
+router.post("/report", CreateComplaint);
 
 module.exports = router;
