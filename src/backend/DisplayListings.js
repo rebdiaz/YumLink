553a4f8a20a2cdbb1f8005dcb62b0d53119f9485
rebdiaz <95255349+rebdiaz@users.noreply.github.com>
2024-04-10 11:51:25 -0400

    Update sort listings

--- src/backend/DisplayListings.js
+++ src/backend/DisplayListings.js
@@ -2,114 +2,82 @@ const Listing = require("./Listings");
 
 
 module.exports.DisplayListings = async (req, res, next) => {
-    let index;
+    let lists;
     try {
-        const {sorted, lowToHigh, highToLow, title, breakfast, lunch, dinner, dessert, beverage, snack,
+        const {
+            sorted, lowToHigh, highToLow, title, breakfast, lunch, dinner, dessert, beverage, snack,
             vegetarian, vegan, other, cuisine, dairyAllergy, glutenAllergy, shellfishAllergy,
-            nutAllergy, soyAllergy, fishAllergy, otherAllergy} = req.body;
+            nutAllergy, soyAllergy, fishAllergy, otherAllergy
+        } = req.body;
 
-        const lists = [];
-        index = 0;
-        if(sorted){
-            if(title !== ''){
-                await Listing.find({title: title}, function (err, results) {
-                    lists[index++] = results;
-                });
+        lists = [];
+        if (sorted) {
+            if (title !== '') {
+                lists.push(await Listing.find({title: title}, function (err, results) {}));
             }
-            if(breakfast !== ''){
-                await Listing.find({breakfast: breakfast}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (breakfast !== '') {
+                lists.push(await Listing.find({breakfast: breakfast}, function (err, results) {}));
             }
-            if(lunch !== ''){
-                await Listing.find({lunch: lunch}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (lunch !== '') {
+                lists.push(await Listing.find({lunch: lunch}, function (err, results) {}));
             }
-            if(dinner !== ''){
-                await Listing.find({dinner: dinner}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (dinner !== '') {
+                lists.push(await Listing.find({dinner: dinner}, function (err, results) {}));
             }
-            if(dessert !== ''){
-                await Listing.find({dessert: dessert}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (dessert !== '') {
+                lists.push(await Listing.find({dessert: dessert}, function (err, results) {}));
             }
-            if(beverage !== ''){
-                await Listing.find({beverage: beverage}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (beverage !== '') {
+                lists.push(await Listing.find({beverage: beverage}, function (err, results) {}));
             }
-            if(snack !== ''){
-                await Listing.find({snack: snack}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (snack !== '') {
+                lists.push(await Listing.find({snack: snack}, function (err, results) {}));
             }
-            if(vegetarian !== ''){
-                await Listing.find({vegetarian: vegetarian}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (vegetarian !== '') {
+                lists.push(await Listing.find({vegetarian: vegetarian}, function (err, results) {}));
             }
-            if(vegan !== ''){
-                await Listing.find({vegan: vegan}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (vegan !== '') {
+                lists.push(await Listing.find({vegan: vegan}, function (err, results) {}));
             }
-            if(other !== ''){
-                await Listing.find({other: other}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (other !== '') {
+                lists.push(await Listing.find({other: other}, function (err, results) {}));
             }
-            if(cuisine !== ''){
-                await Listing.find({cuisine: cuisine}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (cuisine !== '') {
+                lists.push(await Listing.find({cuisine: cuisine}, function (err, results) {}));
             }
-            if(dairyAllergy !== ''){
-                await Listing.find({dairyAllergy: dairyAllergy}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (dairyAllergy !== '') {
+                lists.push(await Listing.find({dairyAllergy: dairyAllergy}, function (err, results) {}));
             }
-            if(glutenAllergy !== ''){
-                await Listing.find({glutenAllergy: glutenAllergy}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (glutenAllergy !== '') {
+                lists.push(await Listing.find({glutenAllergy: glutenAllergy}, function (err, results) {}));
             }
-            if(shellfishAllergy !== ''){
-                await Listing.find({shellfishAllergy: shellfishAllergy}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (shellfishAllergy !== '') {
+                lists.push(await Listing.find({shellfishAllergy: shellfishAllergy}, function (err, results) {}));
             }
-            if(nutAllergy !== ''){
-                await Listing.find({nutAllergy: nutAllergy}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (nutAllergy !== '') {
+                lists.push(await Listing.find({nutAllergy: nutAllergy}, function (err, results) {}));
             }
-            if(soyAllergy !== ''){
-                await Listing.find({soyAllergy: soyAllergy}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (soyAllergy !== '') {
+                lists.push(await Listing.find({soyAllergy: soyAllergy}, function (err, results) {}));
             }
-            if(fishAllergy !== ''){
-                await Listing.find({fishAllergy: fishAllergy}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (fishAllergy !== '') {
+                lists.push(await Listing.find({fishAllergy: fishAllergy}, function (err, results) {}));
             }
-            if(otherAllergy !== ''){
-                await Listing.find({otherAllergy: otherAllergy}, function (err, results) {
-                    lists[index++] = results;
-                });
+            if (otherAllergy !== '') {
+                lists.push(await Listing.find({otherAllergy: otherAllergy}, function (err, results) {}));
             }
 
-        } else if(lowToHigh){
-            await Listing.find({}).sort({pricePerUnit: 'asc'});
-        } else if(highToLow){
-            await Listing.find({}).sort({pricePerUnit: 'desc'});
-        } else {
-            await Listing.find({});
+        }  else {
+            lists = await Listing.find({});
         }
 
+        if(lowToHigh){
+            lists.sort(function(a, b){return a.pricePerUnit - b.pricePerUnit});
+            //lists = await Listing.find({}).sort({pricePerUnit: 'asc'});
+        } else if (highToLow) {
+            lists.sort(function(a, b){return b.pricePerUnit - a.pricePerUnit});
+            //lists = await Listing.find({}).sort({pricePerUnit: 'desc'});
+        }
 
 
         res.status(201).json({success: true, message: "Listings found", lists});
