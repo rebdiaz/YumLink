fe4a3eb88667c811a16b7982f16eacfd48f1c250
Anshil Patel <anshilpatel0@gmail.com>
2024-04-16 15:18:40 -0400

    Co-authored-by: satvikvippatoori <satvikvippatoori@users.noreply.github.com>

--- src/frontend/yumlink/src/App.js
+++ src/frontend/yumlink/src/App.js
@@ -8,21 +8,29 @@ import ListingPopUp from './components/ListingPopUp/ListingPopUp'
 import View from './components/View/View'
 
 const App = () => {
+  const appStyle = {
+    backgroundImage: `url("https://t3.ftcdn.net/jpg/06/29/73/36/360_F_629733635_4eo0wfZfYC6u1IKn3Fb3uFMWQD87n6DD.jpg")`, // Set background image URL here
+    backgroundSize: 'cover', // Ensure the background image covers the entire container
+    minHeight: '100vh', // Ensure the background covers the entire viewport height
+  };
+
   return (
     <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/listings" exact component={Listings} />
-        <Route path="/home" component={Home} />
-        <Route path="/login" component={LoginSignup} />
-        <Route path="/create" component={ListingPopUp} />
-        <Route path="/view" component={View} />
-        {/* Define a Route for the login page */}
-        <Route path="/" exact>
-          {/* Render the login page component */}
-          <Redirect to="/login" /> {/* Redirect to the login page */}
-        </Route>
-      </Switch>
+      <div style={appStyle}>
+        <NavBar />
+        <Switch>
+          <Route path="/listings" exact component={Listings} />
+          <Route path="/home" component={Home} />
+          <Route path="/login" component={LoginSignup} />
+          <Route path="/create" component={ListingPopUp} />
+          <Route path="/view" component={View} />
+          {/* Define a Route for the login page */}
+          <Route path="/" exact>
+            {/* Render the login page component */}
+            <Redirect to="/login" /> {/* Redirect to the login page */}
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
